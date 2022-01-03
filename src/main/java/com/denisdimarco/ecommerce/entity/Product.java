0af85b2a601ac5069d0a094d39f769cdbe9ee089
denisdimarco <denisdimarcoleda@gmail.com>
2022-01-02 21:59:10 -0300

    Initial Config

---
+++ src/main/java/com/denisdimarco/ecommerce/entity/Product.java
@@ -0,0 +1,51 @@
+package com.denisdimarco.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal unitPrice;
+
+    private String imageUrl;
+
+    private boolean active;
+
+    private int unitsInStock;
+
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+}
