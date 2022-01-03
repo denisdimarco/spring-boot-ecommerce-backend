ee991d4c886ea0bff40373b5f6ee53edc12e30c5
denisdimarco <denisdimarcoleda@gmail.com>
2022-01-02 22:00:24 -0300

    Create DataRestConfig.java

---
+++ src/main/java/com/denisdimarco/ecommerce/config/DataRestConfig.java
@@ -0,0 +1,33 @@
+package com.denisdimarco.ecommerce.config;
+
+import com.denisdimarco.ecommerce.entity.Product;
+import com.denisdimarco.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] unsupportedAction = {
+                HttpMethod.PUT,
+                HttpMethod.POST,
+                HttpMethod.DELETE,
+        };
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedAction)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedAction));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedAction)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedAction));
+    }
+}
